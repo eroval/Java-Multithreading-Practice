e28eee800282011d80fc753086260ea1c9a03d23
Denis Zangarov <denisimo_98@yahoo.com>
2022-02-10 15:39:41 +0200

    010-Producer_Consumer_Eater_Synchronized

---
+++ Multithreading Pluralsight/010-Producer_Consumer_Eater_Synched/Program010.java
@@ -0,0 +1,99 @@
+import javax.sound.midi.Sequencer.SyncMode;
+
+class SharedSum{
+    private final static Object syncObj = new Object();
+
+    private static SharedSum obj = new SharedSum();
+    private int Sum = 0;
+
+    // check when buffer is full
+    private int maxVal = 2000;
+    private SharedSum(){}
+    private SharedSum(SharedSum obj){}
+
+    public static SharedSum getSum(){
+        synchronized(syncObj){
+            return obj;
+        }
+    }
+
+    public static void incrementSum() throws InterruptedException{
+        synchronized(syncObj){
+            if(obj.Sum==obj.maxVal){
+                Thread.sleep(1000);
+                syncObj.notifyAll();
+                syncObj.wait();
+            }
+            ++obj.Sum;
+        }
+    }
+
+    public static void decrementSum() throws InterruptedException{
+        synchronized(syncObj){
+            if(obj.Sum==0){
+                Thread.sleep(1000);
+                syncObj.notifyAll();
+                syncObj.wait();
+            }
+            --obj.Sum;
+        }
+    }
+
+    public int Value(){
+        return this.Sum;
+    }
+
+    @Override
+    public String toString() {
+        return "Sum = " + obj.Sum;
+    }
+}
+
+class Producer{
+    public void produce() throws InterruptedException{
+        SharedSum.incrementSum();
+    }
+}
+
+class Consumer{
+    public void consume() throws InterruptedException{
+        SharedSum.decrementSum();
+    }
+}
+
+public class Program010 {
+    private static void produce() {
+        Producer p = new Producer();
+        while(true) {
+            System.out.println(SharedSum.getSum().Value());
+            try {
+                p.produce();
+            } catch (InterruptedException e) { }
+        }
+    }
+    private static void consume() {
+        Consumer c = new Consumer();
+        while(true) {
+            System.out.println(SharedSum.getSum().Value());
+            try {
+                c.consume();
+            } catch (InterruptedException e) { }
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread producerOne = new Thread(()-> produce());
+        Thread consumerOne = new Thread(()-> consume());
+
+        producerOne.start();
+        Thread.sleep(1000);
+        consumerOne.start();
+
+        producerOne.join();
+        consumerOne.join();
+
+        System.out.println(SharedSum.getSum());
+    }
+}
