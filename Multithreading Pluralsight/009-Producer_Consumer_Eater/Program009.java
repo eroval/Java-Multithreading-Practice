dec7890c3f6d988bedb7ff5a63b76f101ad47976
Denis Zangarov <denisimo_98@yahoo.com>
2022-02-10 14:48:42 +0200

    009-Producer_Consumer_Eater_DeadLock

---
+++ Multithreading Pluralsight/009-Producer_Consumer_Eater/Program009.java
@@ -0,0 +1,85 @@
+import javax.sound.midi.Sequencer.SyncMode;
+
+// This will produce a deadlock
+// because the race conditions depend on while clauses
+// which makes this approach obsolete
+
+class SharedSum{
+    private final static Object syncObj = new Object();
+
+    private static SharedSum obj = new SharedSum();
+    private int Sum = 0;
+    private SharedSum(){}
+    private SharedSum(SharedSum obj){}
+
+    public static SharedSum getSum(){
+        // Synchronizing the singleton is not enough
+        synchronized(syncObj){
+            return obj;
+        }
+    }
+
+    public static void incrementSum(){
+        // If we were to delete the syncrhonization, 
+        // annomalies would arise
+        synchronized(syncObj){
+            ++obj.Sum;
+        }
+    }
+
+    public static void decrementSum(){
+        synchronized(syncObj){
+            --obj.Sum;
+        }
+    }
+
+    public int Value(){
+        return this.Sum;
+    }
+
+    @Override
+    public String toString() {
+        return "Sum = " + obj.Sum;
+    }
+}
+
+class Producer{
+    public void produce(){
+        SharedSum.incrementSum();
+    }
+}
+
+class Consumer{
+    public void consume(){
+        SharedSum.decrementSum();
+    }
+}
+
+public class Program009 {
+    private static void produce(){
+        Producer p = new Producer();
+        while(true) p.produce();
+    }
+    private static void consume(){
+        Consumer c = new Consumer();
+        while(SharedSum.getSum().Value()>0) c.consume();;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread producerOne = new Thread(()->produce());
+        Thread producerTwo = new Thread(()->produce());
+        Thread consumerOne = new Thread(()->consume());
+
+        producerOne.start();
+        consumerOne.start();
+        producerTwo.start();
+
+        producerOne.join();
+        consumerOne.join();
+        producerTwo.join();
+
+        System.out.println(SharedSum.getSum());
+    }
+}
