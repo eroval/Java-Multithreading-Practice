2adfdffba6d7f6a85da9eeac80f2b8b4178ed5bb
Denis Zangarov <denisimo_98@yahoo.com>
2022-02-11 15:51:36 +0200

    015-Fixed_Double_Checking

---
+++ Multithreading Pluralsight/015-Fixed_Double_Checking/Program015.java
@@ -0,0 +1,85 @@
+class Singleton{
+    private volatile static Singleton instance;
+    private String name = "Denis";
+    private int age = 0;
+
+    private Singleton(){}
+    private Singleton(Singleton singleton){}
+    
+    //locking objects
+    private final Object keyName = new Object();
+    private final Object keyAge = new Object();
+    private final static Object lock = new Object();
+
+    public static Singleton get(){
+        if(instance!=null){
+            return instance;
+        }
+
+        synchronized(lock){
+            if(instance==null){
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+
+    public void setAge(int age){
+        synchronized(this.keyAge){
+            this.age=age;
+        }
+    }
+
+    public int getAge(){
+        synchronized(this.keyAge){
+            return this.age;
+        }
+    }
+
+    public void setName(String name){
+        synchronized(this.keyName){
+            this.name=name;
+        }
+    }
+
+    public String getName(){
+        synchronized(this.keyName){
+            return this.name;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name = " + this.name + "\nAge = " + this.age; 
+    }
+}
+
+
+public class Program015{
+    public static void main(String[] args) throws InterruptedException {
+        Runnable a = () -> { while(!Thread.currentThread().interrupted()) System.out.println(Singleton.get().getAge()); };
+        Runnable b = () -> { while(!Thread.currentThread().interrupted()) System.out.println(Singleton.get().getAge()); };
+
+        int n = 50;
+        Thread[] threads = new Thread[n];
+        for(int i=0; i<n; i++){
+            if(i%2==0){
+                threads[i] = new Thread(a);
+            }
+            else threads[i] = new Thread(b);
+        }
+
+
+
+        for(int i=0; i<n; i++) threads[i].start();
+
+        Singleton.get().setAge(20);
+        Singleton.get().setName("Lano");
+
+
+        for(int i=0; i<n; i++) {
+            threads[i].interrupt();
+            threads[i].join();
+        }
+    }
+}
